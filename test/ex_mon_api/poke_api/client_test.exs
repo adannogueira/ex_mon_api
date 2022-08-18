937d6ab18f0d8c7b74005c36282621a140ce53f9
Adan Nogueira <adan.nogueira@gmail.com>
2022-08-18 08:20:50 -0300

    refacotr: update client test to mock an api call

--- test/ex_mon_api/poke_api/client_test.exs
+++ test/ex_mon_api/poke_api/client_test.exs
@@ -1,19 +1,42 @@
 defmodule ExMonApi.PokeApi.ClientTest do
-  use ExMonApi.DataCase
+  use ExUnit.Case
+
+  import Tesla.Mock
 
   alias ExMonApi.PokeApi.Client
 
+  @base_url "https://pokeapi.co/api/v2/pokemon/"
+
   describe "get_pokemon/1" do
     test "should return pokemon data when the pokemon exists" do
+      body = %{"name" => "pikachu", "weight" => 60, "types" => ["electric"]}
+      mock(fn %{method: :get, url: @base_url <> "pikachu"} ->
+        %Tesla.Env{status: 200, body: body}
+      end)
+
       {:ok, response} = Client.get_pokemon("pikachu")
 
-      assert %{"name" => "pikachu", "weight" => 60} = response
+      assert body == response
     end
 
     test "should return an error when the pokemon doesn't exist" do
+      mock(fn %{method: :get, url: @base_url <> "invalid_pokemon"} ->
+        %Tesla.Env{status: 404}
+      end)
+
       {:error, response} = Client.get_pokemon("invalid_pokemon")
 
       assert %{message: "Pokemon not found!", status: 404} = response
+    end
+
+    test "should return an error when there's an unexpected error" do
+      mock(fn %{method: :get, url: @base_url <> "pikachu"} ->
+        {:error, :timeout}
+      end)
+
+      {:error, response} = Client.get_pokemon("pikachu")
+
+      assert :timeout == response
     end
   end
 end
