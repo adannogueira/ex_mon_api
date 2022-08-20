dc7966aa88d1fd4d672fe50c45eb382dae7ae683
Adan Nogueira <adan.nogueira@gmail.com>
2022-08-20 14:21:39 -0300

    feat: create authentication function

--- lib/ex_mon_api_web/auth/guardian.ex
+++ lib/ex_mon_api_web/auth/guardian.ex
@@ -1,6 +1,8 @@
 defmodule ExMonApiWeb.Auth.Guardian do
   use Guardian, otp_app: :ex_mon_api
 
+  alias ExMonApi.{Repo, Trainer}
+
   def subject_for_token(%{id: id}, _claims) do
     sub = to_string(id)
     {:ok, sub}
@@ -9,5 +11,24 @@ defmodule ExMonApiWeb.Auth.Guardian do
   def resource_from_claims(%{"sub" => id}) do
     resource = ExMonApi.fetch_trainer(id)
     {:ok,  resource}
+  end
+
+  def authenticate(%{"id" => trainer_id, "password" => password}) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, %{message: "Trainer not found!", status: 404}}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  def validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  defp create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
   end
 end
